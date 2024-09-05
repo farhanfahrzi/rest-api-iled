9206a690f66e7f7cb24d234bd3cc26076c8fef02
jendraluqmanzenUT <054563392@ecampus.ut.ac.id>
2024-09-05 09:43:22 +0700

    [main] menambah file main

---
+++ src/test/java/com/enigma/Instructor_Led/InstructorLedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.Instructor_Led;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstructorLedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
